2f737849eb75ddcbcc2064c827a0f7a72ef36e79
bgonzalezfractal <bgonzalez@fractal-bi.com>
2021-01-08 23:47:43 -0300

    fixes for backend and order for docker

--- rose_dev/social_auth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import GoogleSocialAuthView, MicrosoftSocialAuthView
-
-urlpatterns = [
-    path('google/', GoogleSocialAuthView.as_view()),
-    path('microsoft/', MicrosoftSocialAuthView.as_view()),
-]
