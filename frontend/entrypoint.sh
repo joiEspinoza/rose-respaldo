a5178564d6b575f540d1f608c5ce2cbf1de5e7cc
bgonzalezfractal <bgonzalez@fractal-bi.com>
2021-01-22 19:44:29 -0300

    fix, changed yarn for npm run build

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cd /app/frontend
+echo "changing localhost to BACKEND endpoint"
 find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/127.0.0.1:8000/https:\/\/${BACK_URL}/g" 
 find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/localhost:8000/https:\/\/${BACK_URL}/g" 
-yarn build
+echo "Building with npm run build"
+npm run build
 rsync -auv --delete ./build /data/frontend/
 
