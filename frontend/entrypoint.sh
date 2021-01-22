bc08b1004a99c05cfa181d9b8914988c50b48a1d
bgonzalezfractal <bgonzalez@fractal-bi.com>
2021-01-22 19:25:22 -0300

    replaced alpine method for slim-buster / image problem

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/127.0.0.1:8000/https:\/\/${BACK_URL}/g" \
-&& find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/localhost:8000/https:\/\/${BACK_URL}/g" \
-&& sleep 2 \
-&& yarn build
-&& rsync -auv --delete ./build /data/frontend/
+find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/127.0.0.1:8000/https:\/\/${BACK_URL}/g" 
+find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/localhost:8000/https:\/\/${BACK_URL}/g" 
+sleep 2
+yarn build
+rsync -auv --delete ./build /data/frontend/
 
