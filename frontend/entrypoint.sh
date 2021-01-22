7bb6ea9781aff969503b3946a4e69048cbef47e9
bgonzalezfractal <bgonzalez@fractal-bi.com>
2021-01-22 19:36:58 -0300

    fix

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-
+cd /app/frontend
 find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/127.0.0.1:8000/https:\/\/${BACK_URL}/g" 
 find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/localhost:8000/https:\/\/${BACK_URL}/g" 
-sleep 2
 yarn build
 rsync -auv --delete ./build /data/frontend/
 
