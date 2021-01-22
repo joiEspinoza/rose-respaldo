001791a8473dc19c5c7a139f8a12de6e6bf770b3
bgonzalezfractal <bgonzalez@fractal-bi.com>
2021-01-22 19:59:24 -0300

    Stuck: npm builds

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,7 +3,8 @@ cd /app/frontend
 echo "changing localhost to BACKEND endpoint"
 find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/127.0.0.1:8000/https:\/\/${BACK_URL}/g" 
 find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/localhost:8000/https:\/\/${BACK_URL}/g" 
-echo "Building with npm run build"
-npm run build
+echo "Building with yarn"
+yarn
+yarn build
 rsync -auv --delete ./build /data/frontend/
 
