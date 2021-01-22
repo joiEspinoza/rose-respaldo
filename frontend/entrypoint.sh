cc943fa50fe7c6a35796c147e011f26d7308fe17
bgonzalezfractal <bgonzalez@fractal-bi.com>
2021-01-22 20:52:10 -0300

    run build deprecated, changed for yarn start

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -5,6 +5,6 @@ find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/127.0.0.1:8000/https:\/\
 find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/localhost:8000/https:\/\/${BACK_URL}/g" 
 echo "Building with yarn"
 yarn
-yarn build
-rsync -auv --delete ./build /data/frontend/
+yarn start
+
 
