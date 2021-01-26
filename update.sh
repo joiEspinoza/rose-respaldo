c0958a35b39280d5ff367974319b2c69ca218468
bgonzalezfractal <bgonzalez@fractal-bi.com>
2021-01-26 20:04:05 -0300

    fix: nginx remove and create

--- update.sh
+++ update.sh
@@ -10,8 +10,7 @@ aws ecr get-login-password --region us-east-2 | docker login --username AWS --pa
 echo "BRANCH: ${BRANCH}"
 
 cd ${ROSE_HOME}
-
+docker rm -f nginx_proxy
 docker-compose pull backend_${BRANCH} frontend_${BRANCH} \
-&& docker rm -f nginx_proxy
 && docker-compose up -d
 #docker-compose pull 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0/master:${BRANCH}
