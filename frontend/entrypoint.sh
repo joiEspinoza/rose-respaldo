a8034b2763a4cc3e84517ecbcca6d5ded2cac502
bgonzalezfractal <bgonzalez@fractal-bi.com>
2021-01-26 20:29:43 -0300

    sed for localhost 3000 replace for $front_url

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,6 +3,7 @@ cd /app/frontend
 echo "changing localhost to BACKEND endpoint"
 find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/127.0.0.1:8000/https:\/\/${BACK_URL}/g" 
 find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/localhost:8000/https:\/\/${BACK_URL}/g" 
+find ./*  -type f -print0 | xargs -0 sed -i "s/http:\/\/localhost:3000/https:\/\/${FRONT_URL}/g" 
 echo "Building with yarn"
 yarn
 yarn start
