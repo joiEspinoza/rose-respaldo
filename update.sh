db7bb6039568b0060793801c3fec730f206817f9
bgonzalezfractal <bgonzalez@fractal-bi.com>
2021-01-26 19:04:24 -0300

    testing flow

--- update.sh
+++ update.sh
@@ -12,5 +12,5 @@ echo "BRANCH: ${BRANCH}"
 cd ${ROSE_HOME}
 
 docker-compose pull backend_${BRANCH} frontend_${BRANCH} \
-&& docker-composeup -d
+&& docker-compose up -d
 #docker-compose pull 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0/master:${BRANCH}
