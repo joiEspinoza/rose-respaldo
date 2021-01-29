314565e3a86b8d50ea10a019cb6b35f387dc02cc
bgonzalezfractal <bgonzalez@fractal-bi.com>
2021-01-29 04:14:56 -0300

    fixed sed mispelled

--- backend/rose_dev/entrypoint.sh
+++ backend/rose_dev/entrypoint.sh
@@ -7,7 +7,7 @@ echo "collecting statics."
 python manage.py collectstatic --noinput
 
 echo "changing selection/tmp to absolute path"
-find ./*  -type f -print0 | xargs -0 sed -i "s/selection\/tmp\\/home\/ec2-user\/rose\/backend\/rose_dev\/selection\/tmp/g" 
+find ./*  -type f -print0 | xargs -0 sed -i "s/selection\/tmp/\/home\/ec2-user\/rose\/backend\/rose_dev\/selection\/tmp/g" 
 
 gunicorn rose_dev.wsgi:application \
     ${GUNICORN_COMMANDS}
