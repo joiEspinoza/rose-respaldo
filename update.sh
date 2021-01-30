5e3ac73cf17aa17108ee25e8cc91426d31ae0632
EC2 Default User <ec2-user@ip-172-31-41-94.us-east-2.compute.internal>
2021-01-30 23:56:48 +0000

    fix update.sh

--- update.sh
+++ update.sh
@@ -5,12 +5,12 @@ export BRANCH=$1
 
 
 #Login docker AWS
-sudo aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0
 
 echo "BRANCH: ${BRANCH}"
 
 cd ${ROSE_HOME}
-sudo docker rm -f nginx_proxy
-sudo docker-compose pull backend_${BRANCH} frontend_${BRANCH}
-sudo docker-compose up -d
-#docker-compose pull 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0/master:${BRANCH}
+docker rm -f nginx_proxy
+docker-compose pull backend_${BRANCH} frontend_${BRANCH}
+docker-compose up -d
+
