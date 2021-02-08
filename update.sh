e9ee44f09759394db89de0468821727406740e22
EC2 Default User <ec2-user@ip-172-31-41-94.us-east-2.compute.internal>
2021-02-08 19:57:01 +0000

    final fix: added git pull + kept docker compose version for swarm

--- update.sh
+++ update.sh
@@ -3,14 +3,14 @@
 ROSE_HOME=/home/ec2-user/rose
 export BRANCH=$1
 
-git pull origin ${BRANCH}
-
 #Login docker AWS
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0
 
 echo "BRANCH: ${BRANCH}"
 
 cd ${ROSE_HOME}
+git pull https://bgonzalezfractal:Fractal1527@bitbucket.org/bgonzalezfractal/rose.git ${BRANCH}
+
 docker rm -f nginx_proxy backend_dev frontend_dev
 docker-compose pull backend_${BRANCH} frontend_${BRANCH}
 docker-compose up -d
