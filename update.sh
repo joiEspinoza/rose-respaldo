15e308e3103df20bb84d4cf50aae9668699742ac
EC2 Default User <ec2-user@ip-172-31-41-94.us-east-2.compute.internal>
2021-02-08 18:11:15 +0000

    fix: update.sh with docker run, fix volumes with docker compose

--- update.sh
+++ update.sh
@@ -10,7 +10,11 @@ aws ecr get-login-password --region us-east-2 | docker login --username AWS --pa
 echo "BRANCH: ${BRANCH}"
 
 cd ${ROSE_HOME}
-docker rm -f nginx_proxy
-docker-compose pull backend_${BRANCH} frontend_${BRANCH}
-docker-compose up -d
+docker rm -f nginx_proxy backend_dev frontend_dev
+docker pull 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0/backend:${BRANCH}
+docker pull 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0/frontend:${BRANCH}
+docker run -d --name=backend_dev --env-file=.env.dev -p=1335:1335 --restart=unless-stopped 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0/backend:${BRANCH}
+docker run -d --name=frontend_dev --env-file=.env_f.dev -p=1337:1337 --restart=unless-stopped 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0/frontend:${BRANCH}
+#docker-compose pull backend_${BRANCH} frontend_${BRANCH}
+#docker-compose up -d
 
