9b4b2e1630e3b4f054718466054133f32c6c3b78
EC2 Default User <ec2-user@ip-172-31-41-94.us-east-2.compute.internal>
2021-02-08 19:41:21 +0000

    fix update.sh added git pull for docker-compose volumes

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 ROSE_HOME=/home/ec2-user/rose
 export BRANCH=$1
 
+git pull origin ${BRANCH}
 
 #Login docker AWS
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0
@@ -11,10 +12,5 @@ echo "BRANCH: ${BRANCH}"
 
 cd ${ROSE_HOME}
 docker rm -f nginx_proxy backend_dev frontend_dev
-docker pull 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0/backend:${BRANCH}
-docker pull 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0/frontend:${BRANCH}
-docker run -d --name=backend_dev --env-file=.env.dev -p=1335:1335 --restart=unless-stopped 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0/backend:${BRANCH}
-docker run -d --name=frontend_dev --env-file=.env_f.dev -p=1337:1337 --restart=unless-stopped 943423409941.dkr.ecr.us-east-2.amazonaws.com/rosev0/frontend:${BRANCH}
-#docker-compose pull backend_${BRANCH} frontend_${BRANCH}
-#docker-compose up -d
-
+docker-compose pull backend_${BRANCH} frontend_${BRANCH}
+docker-compose up -d
